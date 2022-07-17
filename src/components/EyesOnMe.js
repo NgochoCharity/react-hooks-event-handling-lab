4a180acae7712bada6fed23b17b72891b9dabe04
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-07-17 16:34:10 +0300

    lab complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+
+import React from "react"
+
+function EyesOnMe(){
+    // function handleFocus(){
+    //     console.log("Good! ")
+    // }
+
+    // function handleBlur(){
+    //     console.log('Hey! Eyes on me! ')
+    // }
+return(
+    <button onFocus={() => console.log("Good!")} onBlur={() =>console.log('Hey! Eyes on me!')} >Eyes on me</button>
+);
+}
+
+export default EyesOnMe;
